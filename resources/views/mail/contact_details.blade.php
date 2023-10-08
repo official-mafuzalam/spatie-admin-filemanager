6db9c10f51256565f871395d087020d58693b582
Mafuz Alam <everythinggood885@gmail.com>
2023-10-09 04:12:19 +0600

    env-change

---
+++ resources/views/mail/contact_details.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Deposit Confirmation</title>
+</head>
+
+<body>
+    <h1>Name: {{ $name }}</h1>
+    <h2>Subject: {{ $subject }}</h2>
+    <h4>Email: {{ $email }}</h4>
+
+    <p>Message: {{ $emailMessage }}</p>
+</body>
+
+</html>
